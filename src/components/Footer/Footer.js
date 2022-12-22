4f647fce4d35912961233d74065177c156f68143
unknown <sidhubasho99@gmail.com>
2022-12-23 00:29:51 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+
+        
+<div class="container-fluid">
+
+  <footer class="text-center text-lg-start" Style="background-color: black;">
+    <div class="container d-flex justify-content-center py-5">
+      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" Style="background-color: black;">
+        <i class="fab fa-facebook-f"></i>
+      </button>
+      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" Style="background-color:black;">
+        <i class="fab fa-youtube"></i>
+      </button>
+      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" Style="background-color: black;">
+        <i class="fab fa-instagram"></i>
+      </button>
+      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" Style="background-color: black;">
+        <i class="fab fa-twitter"></i>
+      </button>
+      <button type="button" class="btn btn-primary btn-lg btn-floating mx-2" Style="background-color: black;">
+        <i class="fab fa-tiktok"></i>
+      </button>
+    </div>
+
+  
+    <div class="text-center text-white p-3" Style="background-color: #181820;">
+      © 2023 
+      <a class="text-white" href="https://mdbootstrap.com/"/> Pixel Te.co
+    </div>
+    
+  </footer>
+  
+</div>
+ 
+  )
+}
